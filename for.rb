0eaf09cc4622aa58291de7c5f4901df733e07f3a
Angelina Montanez <angelinamontanez80@gmail.com>
2019-02-03 01:21:15 +0000

    Automatically backed up by Learn

--- for.rb
+++ for.rb
@@ -2,10 +2,8 @@
 def using_for
 	checklist = 1..10
 	#yoshoe_count = 1..32
- 
-for shoe in shoe_count 
+
+for shoe in shoe_count
     puts "I'm putting a pair of shoes away."
 endur code here
 end
-
-
